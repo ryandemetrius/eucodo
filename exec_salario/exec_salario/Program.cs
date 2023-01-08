d48ad91898b2717e34a3f5cd121664231f0e7060
Ryan Borges <ryan.dob14@gmail.com>
2023-01-08 17:30:38 -0300

    Initial commit

---
+++ exec_salario/exec_salario/Program.cs
@@ -0,0 +1,34 @@
+﻿
+/*
+E: &&
+OU: ||
+NAO: !
+*/
+
+Console.WriteLine("Insira sua idade:");
+int idade = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Qual seu sexo? M para Masculino e F para Feminino");
+string sexo = Console.ReadLine();
+
+Console.WriteLine("Qual seu salário?");
+double salario = double.Parse(Console.ReadLine());
+
+/*
+Se for maior que 20 anos e for mulher, salario aumenta 50%
+Se for maior que 30 anos e for homem, salario aumenta em 10%
+*/
+
+if(sexo == "M" && idade > 30)
+{   
+    salario = salario * 1.1;
+}
+
+if(sexo == "F" && idade > 20)
+{
+    salario = salario * 1.5;
+}
+
+Console.WriteLine("seu salário é igual a: " + salario);
+
+Console.ReadKey();
