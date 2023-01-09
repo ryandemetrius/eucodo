7a92760779a926037278a55a33889eb8cd5fce80
Ryan Borges <ryan.dob14@gmail.com>
2023-01-08 23:20:17 -0300

    Calcular média de notas do semestre

---
+++ exec_notas/Program.cs
@@ -0,0 +1,20 @@
+﻿
+Console.WriteLine("Insira sua nota do primeiro semestre:");
+double nota1 = double.Parse(Console.ReadLine());
+
+Console.WriteLine("Insira sua nota do segundo semestre:");
+double nota2 = double.Parse(Console.ReadLine());
+
+double mediaAno = ((nota1 + nota2) / 2);
+double pontosFaltou = (70 - mediaAno);
+
+if (mediaAno >= 70)
+{
+    Console.WriteLine("Parabéns! Você foi aprovado com uma média anual de " + mediaAno + ".");
+
+} else
+{
+    Console.WriteLine("Você foi reprovado! Faltaram " + pontosFaltou + " pontos para você ser aprovado.");
+}
+
+Console.ReadKey();
