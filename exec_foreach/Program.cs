b711e0b223b79c6ae197efdcc74d6d404bbdba47
Ryan Borges <ryan.dob14@gmail.com>
2023-01-13 07:21:00 -0300

    Inserindo mais exercícios

---
+++ exec_foreach/Program.cs
@@ -0,0 +1,33 @@
+﻿
+using System.Runtime.CompilerServices;
+/*
+ int[] numeros = new int[] { 1, 2, 3, 4, 5 };
+
+// While
+int i = 1;
+while (i < numeros.Length)
+{
+    Console.WriteLine(numeros[i]);
+    i ++;
+}
+
+// For
+for(i = 1; i < numeros.Length; i ++)
+{
+    Console.WriteLine(numeros[i]);
+}
+
+// ForEach
+foreach(var alo in numeros)
+{
+    Console.WriteLine(alo);
+}
+*/
+
+Console.WriteLine("Digite o texto:");
+string texto = Console.ReadLine();
+
+foreach(char letra in texto)
+{
+    Console.WriteLine(letra);
+}
---
+++ exec_parametroOpcionsl/Program.cs
@@ -0,0 +1,34 @@
+﻿
+/* exercicio resolvido sem parâmetro opcional (mais facil eu acho)
+try
+{
+    Console.WriteLine("Informe sua idade:");
+    int idade = int.Parse(Console.ReadLine());
+}
+catch
+{
+    Console.WriteLine("Informe um texto.");
+}
+*/
+
+class meuPrograma
+{
+    static void Main()
+    {
+        string nome = ObterInformacao("Informe seu nome");
+        string idade = ObterInformacao("Informe sua idade");
+        string nomeCachorro = ObterInformacao();
+
+        Console.WriteLine("Nome: {0}", nome);
+        Console.WriteLine("Idade: {0}", idade);
+        Console.WriteLine("Nome do cachorro: {0}", nomeCachorro);
+
+    }
+
+    static string ObterInformacao(string texto = "Informe um texto")
+    {
+        Console.WriteLine(texto + ":");
+        return Console.ReadLine();
+    }
+}   
+
