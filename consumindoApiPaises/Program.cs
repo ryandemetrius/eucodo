26ad1796ea9f0fb6023ad83940ce2986b21ff1ff
ryandemetrius <ryan.dob14@gmail.com>
2023-07-08 20:30:21 -0300

    Commit exercícios com APIs

---
+++ consumindoApiPaises/Program.cs
@@ -0,0 +1,11 @@
+﻿
+using RestSharp;
+
+Console.WriteLine("Digite o nome do país:");
+string país = Console.ReadLine();
+
+var client = new RestClient("https://restcountries.com");
+var request = new RestRequest($"/v3.1/name/{país}", Method.Get);
+var response = client.Execute(request);
+
+Console.WriteLine(response.Content);
